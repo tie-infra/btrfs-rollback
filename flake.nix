6c78d639dc137a2b9852d8597e2c2a76d50babb8
Ivan Trubach <mr.trubach@icloud.com>
2025-06-26 10:15:43 +0300

    chore: clean up flake dependencies

--- flake.nix
+++ flake.nix
@@ -3,28 +3,37 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    systems.url = "systems";
+
     flake-parts.url = "flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     minimal-shell.url = "github:tie-infra/minimal-shell";
+    minimal-shell.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import inputs.systems;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-    flake.overlays.default = import ./overlay.nix;
+      flake.overlays.default = import ./overlay.nix;
 
-    perSystem = { lib, pkgs, ... }: {
-      apps = lib.mkIf (pkgs.stdenv.isLinux) {
-        default.program =
-          (pkgs.extend inputs.self.overlays.default).btrfs-rollback;
-      };
-      checks = lib.mkIf (pkgs.stdenv.isLinux) {
-        default = inputs.nixpkgs.lib.nixos.runTest {
-          imports = [ ./test.nix ];
-          hostPkgs = pkgs;
-          defaults.nixpkgs.overlays = [ inputs.self.overlays.default ];
+      perSystem =
+        { lib, pkgs, ... }:
+        {
+          apps = lib.mkIf (pkgs.stdenv.isLinux) {
+            default.program = (pkgs.extend inputs.self.overlays.default).btrfs-rollback;
+          };
+          checks = lib.mkIf (pkgs.stdenv.isLinux) {
+            default = inputs.nixpkgs.lib.nixos.runTest {
+              imports = [ ./test.nix ];
+              hostPkgs = pkgs;
+              defaults.nixpkgs.overlays = [ inputs.self.overlays.default ];
+            };
+          };
         };
-      };
     };
-  };
 }
